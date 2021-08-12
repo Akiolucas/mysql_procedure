4bdec671861c98bc6200490f9c4b7d6f39fba6a0
Lucas Akio <lucasakio.td@gmail.com>
2021-08-12 12:04:18 -0300

    exemplo de como deve ser feito para chamar a procedure

---
+++ chamar-procedure.sql
@@ -0,0 +1,12 @@
+/*
+Parâmetros a serem passados nessa ordem e respeitado o tipo!
+
+nome varchar(30), 
+preco double ,
+kilograma double,
+quantidade int
+
+*/
+/*Exemplos: chame a procedure e passe os parâmetros */
+call cadastrar_codigo_produto_estoque('Açaí',35,2,35);
+
